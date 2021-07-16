7286bb675aa45f23b2d100c589011316a0b46a60
marcosvazquez3 <marcosvazquez3@udc.es>
2021-07-16 22:36:31 +0200

    Cambio dos test do modelo polo cambio que houbo no modelo

--- AnalisisPaqueteria/catalog/test_model.py
+++ AnalisisPaqueteria/catalog/test_model.py
@@ -1,13 +1,19 @@
 import unittest
 from django.test import TestCase
 from pcapinspector.models import PcapInfo
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.models import AnonymousUser, User
+#from django.contrib.auth.models.AnonymousUser import delete
 
 
 class ModelsTestCase(TestCase):
-    # Fixture: preparación de los datos a probar
+    
     def setUp(self):
-        PcapInfo.objects.create(frame_number=1, frame_time="Apr 25, 2021 22:55:03.226624000 CEST", eth_src=None, eth_dst=None, ip_src="10.215.173.1", ip_dst="10.215.173.2", src_port=21039, dst_port=53, ttl=64, protocol="DNS", ip_len=60)
-        PcapInfo.objects.create(frame_number=2, frame_time="Apr 25, 2021 23:55:03.226624000 CEST", eth_src=None, eth_dst=None, ip_src="10.215.173.2", ip_dst="10.215.173.3", src_port=21039, dst_port=53, ttl=64, protocol="DNS", ip_len=60)
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
+        PcapInfo.objects.create(frame_number=1, frame_time="Apr 25, 2021 22:55:03.226624000 CEST", eth_src=None, eth_dst=None, ip_src="10.215.173.1", ip_dst="10.215.173.2", src_port=21039, dst_port=53, ttl=64, protocol="DNS", ip_len=60, user=self.user)
+        PcapInfo.objects.create(frame_number=2, frame_time="Apr 25, 2021 23:55:03.226624000 CEST", eth_src=None, eth_dst=None, ip_src="10.215.173.2", ip_dst="10.215.173.3", src_port=21039, dst_port=53, ttl=64, protocol="DNS", ip_len=60, user=self.user)
+
 
     def test_products(self):
         # Recuperamos los dos productos y comprobamos sus campos
@@ -41,3 +47,6 @@ class ModelsTestCase(TestCase):
     def test_product(self):
         # Test que debe fallar porque no hay tantas reviews
         r = PcapInfo.objects.get(id=2434234)
+
+    def deletBD(self):
+        PcapInfo.objects.filter(user=self.user).delete()
